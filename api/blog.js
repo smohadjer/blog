2851446325090d00701bbbb0435f67cb404cf29f
Saeid Mohadjer <saeid@fastmail.fm>
2023-12-14 16:41:10 +0100

    update

--- api/blog.js
+++ api/blog.js
@@ -5,12 +5,6 @@ import client from './_db.js';
 import { ObjectId } from 'mongodb';
 import marked from 'marked';
 
-function getFileContent(filename) {
-  const absolutePath = path.join(process.cwd(), 'public', 'json', filename);
-  const fileContent = fs.readFileSync(absolutePath, 'utf8');
-  return fileContent;
-};
-
 function getTemplate() {
     const pathHbs = path.join(process.cwd(), 'public', 'blog.hbs');
     const template = fs.readFileSync(pathHbs, 'utf8');
@@ -23,7 +17,6 @@ export default async (req, res) => {
         await client.connect();
         const database = client.db('blog');
         const collection = database.collection('demo-posts');
-        console.log('slug:', req.query.slug);
         const slug = req.query.slug;
         const query = (slug && slug.length > 0) ? {'slug': slug} : {};
 
@@ -39,9 +32,9 @@ export default async (req, res) => {
 
             res.setHeader('Content-Type', 'text/html; charset=utf-8');
             res.end(markup);
-          }
+        }
 
-          if (req.method === 'POST') {
+        if (req.method === 'POST') {
             const obj = await insertPost(posts, req);
             if (obj.id) {
               // create or update blog files on github using github API
@@ -57,20 +50,10 @@ export default async (req, res) => {
                 message: obj.error || 'Invalid data!'
               });
             }
-          }
-
+        }
     } catch (e) {
         console.error(e);
     } finally {
         await client.close();
     }
-
-    // const postId = req.query.id;
-    // const json = getFileContent('data.json');
-
-    // const jsonData = JSON.parse(json);
-
-    // const posts = postId ? jsonData.filter(item => item.id === req.query.id)
-    //     : jsonData;
-
 }
