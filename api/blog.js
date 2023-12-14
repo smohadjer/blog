98ba94c1566e933ebb0430dafdf416bdf1f46063
Saeid Mohadjer <saeid@fastmail.fm>
2023-12-14 12:19:48 +0100

    bug fix

--- api/blog.js
+++ api/blog.js
@@ -2,7 +2,7 @@ import path from 'path';
 import fs from 'fs';
 
 export function getFileContent(filePath) {
-  const absolutePath = path.join(process.cwd(), filePath);
+  const absolutePath = path.join(process.cwd(), 'public', filePath);
   console.log(absolutePath);
   const fileContent = fs.readFileSync(absolutePath, 'utf8');
   return fileContent;
@@ -10,7 +10,7 @@ export function getFileContent(filePath) {
 
 export default async (req, res) => {
     const postId = req.query.id;
-    const json = getFileContent('public/json/data.json');
+    const json = getFileContent('json/data.json');
 
     const jsonData = JSON.parse(json);
 
