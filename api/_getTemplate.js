8d69e3cffeb98086a6267208ec83bdc73bca69f7
Saeid Mohadjer <saeid@fastmail.fm>
2024-08-21 11:29:54 +0200

    removes header and footer templates

--- api/_getTemplate.js
+++ api/_getTemplate.js
@@ -4,15 +4,7 @@ import Handlebars from 'handlebars';
 
 export default (filename) => {
     const pathHbs = path.join(process.cwd(), 'templates', filename);
-    const footerHbs = path.join(process.cwd(), 'templates', 'footer.hbs');
-    const headerHbs = path.join(process.cwd(), 'templates', 'header.hbs');
     const template = fs.readFileSync(pathHbs, 'utf8');
-    const footer = fs.readFileSync(footerHbs, 'utf8');
-    const header = fs.readFileSync(headerHbs, 'utf8');
-
-    Handlebars.registerPartial('footer', footer);
-    Handlebars.registerPartial('header', header);
-
     const compiledTemplate = Handlebars.compile(template);
     return compiledTemplate;
 }
