cd1cee267656bc8a2b75dd946fad091cec6d2096
Saeid Mohadjer <saeid@fastmail.fm>
2024-08-22 00:07:52 +0200

    adds deleting posts

--- api/blog.ts
+++ api/blog.ts
@@ -3,8 +3,28 @@ import getPosts from './_getPosts.js';
 import insertPost from './_insertPost.js';
 import getTags from './_getTags.js';
 import getMarkup from './_getMarkup.js';
+import { ObjectId } from 'mongodb';
 
-export default async (req, res) => {
+type RequestBody = {
+    date: string;
+    title: string;
+    slug: string;
+    content: string;
+    summary: string;
+    permission: string;
+    tags: string;
+    post_id: string;
+  }
+
+type Request = {
+    body: RequestBody;
+    method: string;
+    query: {
+        response: string;
+    };
+}
+
+export default async (req: Request, res) => {
     try {
         await client.connect();
         const database = client.db('blog');
@@ -27,18 +47,22 @@ export default async (req, res) => {
         if (req.method === 'POST') {
             const obj = await insertPost(req, collection);
             if (obj.id) {
-            //   res.status(200).send({
-            //     message: 'Post inserted!',
-            //     post_id: obj.id
-            //   });
                 res.setHeader('Content-Type', 'text/html; charset=utf-8');
-                res.send(`<p>Post successful! Post ID: ${obj.id}<br><a href="/${obj.doc.slug}">${obj.doc.title}</a>`);
+                res.send(`<p>Post successful! Post ID: ${obj.id}<br><a href="/${obj.doc.slug}">${obj.doc.title}</a></p>`);
             } else {
               res.status(500).send({
                 error: 500,
                 message: obj.error || 'Invalid data!'
               });
             }
+        }
+
+        if (req.method === 'DELETE') {
+            const query = {_id: new ObjectId(req.body.post_id)};
+            const doc = await collection.find(query).toArray();
+            const deleteResult = await collection.deleteOne(doc[0]);
+            res.setHeader('Content-Type', 'text/html; charset=utf-8');
+            res.send(`<p>Deleted ${deleteResult.deletedCount} documents!</p`);
         }
     } catch (e) {
         console.error(e);
